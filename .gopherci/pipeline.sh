680dfea39d8052839b37bc50accec6e89a49d7e4
David Parsley <dlp7y@virginia.edu>
2019-02-01 13:14:54 -0500

    Add pipeline for updating github

---
+++ .gopherci/pipeline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source $GOPHER_INSTALLDIR/lib/gopherbot_v1.sh
+
+FailTask dmnotify khs3z "singularity-scripts pipeline failed, emailing job history..."
+FailCommand builtin-history "send history $GOPHER_JOB_NAME:$GOPHER_NAMESPACE_EXTENDED/$GOPHERCI_BRANCH $GOPHER_RUN_INDEX to khs3z@virginia.edu"
+
+CHILDREN=*
+
+mkdir -p github
+
+for CHILD in $CHILDREN
+do
+    [ -d $CHILD ] && AddTask exec .gopherci/update.sh $CHILD
+done
---
+++ .gopherci/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+$REPO=$1
+
+echo "Cloning $REPO"
+git clone git@github.com:arcsUVA/$REPO.git github/$REPO
+echo "Cleaning out $REPO and updating contents"
+rm -f github/$REPO/Singularity.*
+cp -a $REPO/Singularity.* github/$REPO
+cd github/$REPO
+echo "Pushing updates (if any)"
+git commit -am "Automated updates from Gir" && git push || echo "(no updates)"
